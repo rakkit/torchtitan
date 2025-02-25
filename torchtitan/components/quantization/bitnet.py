8457f7aa6f18f8edc30c7c5a417e0cac83853632
janEbert <janpublicebert@posteo.net>
2025-02-25 21:34:10 +0100

    Try to make BitNet work by patching PyTorch

--- torchtitan/components/quantization/bitnet.py
+++ torchtitan/components/quantization/bitnet.py
@@ -42,6 +42,126 @@ class BitNetConverter(ModelConverter):
 
         logger.info("BitNet training active")
 
+    def _patch_pytorch(self):
+        # We manually apply PyTorch commit
+        # 75661f2036d36fd7f869cd749eb6ef5fb40e4772 here.
+
+        import inspect
+        from typing import cast
+
+        import torch
+        from torch.distributed.fsdp._fully_shard._fsdp_common import (
+            _to_dtype_if_needed,
+            compiled_autograd_enabled,
+        )
+        from torch.distributed.fsdp._fully_shard._fsdp_param import (
+            FSDPParam,
+            ShardedState,
+        )
+        from torch.distributed.tensor.device_mesh import _mesh_resources
+
+        if hasattr(FSDPParam, "shard_mesh_from_root"):
+            logger.info("Not patching PyTorch; not necessary")
+            return
+
+        @property
+        def all_gather_inputs(self) -> list[torch.Tensor]:  # 1D
+            self._assert_in_states(
+                ShardedState.SHARDED, ShardedState.SHARDED_POST_FORWARD
+            )
+            if self.sharded_state == ShardedState.SHARDED:
+                if not compiled_autograd_enabled() and hasattr(
+                    self._sharded_local_tensor, "fsdp_pre_all_gather"
+                ):
+                    sharded_local_tensor = self._sharded_local_tensor
+                    if self.offload_to_cpu:
+                        sharded_local_tensor = sharded_local_tensor.to(
+                            self.device, non_blocking=True
+                        )
+                    pre_all_gather_signature = inspect.signature(
+                        sharded_local_tensor.fsdp_pre_all_gather
+                    )
+                    num_fn_params = len(pre_all_gather_signature.parameters)
+                    # Old signature only passes mesh; keep for BC for now
+                    assert num_fn_params in (1, 5,), (
+                        f"Invalid fsdp_pre_all_gather: {pre_all_gather_signature}\n"
+                        "Expects fsdp_pre_all_gather(self, mesh: DeviceMesh, "
+                        "module: nn.Module, mp_policy: MixedPrecisionPolicy)"
+                    )
+                    if num_fn_params == 1:
+                        (
+                            all_gather_inputs,
+                            self._extensions_data.all_gather_metadata,
+                        ) = sharded_local_tensor.fsdp_pre_all_gather(
+                            self.shard_mesh_from_root
+                        )
+                    else:
+                        (
+                            all_gather_inputs,
+                            self._extensions_data.all_gather_metadata,
+                        ) = sharded_local_tensor.fsdp_pre_all_gather(
+                            self.shard_mesh_from_root,
+                            self._orig_size,
+                            self._contiguous_orig_stride,
+                            self._module_info.module,
+                            self.mp_policy,
+                        )
+                        if (
+                            sharded_local_tensor.size()
+                            != self.padded_sharded_param_size
+                            and any(
+                                all_gather_input.size()
+                                != self.padded_sharded_param_size
+                                for all_gather_input in all_gather_inputs
+                            )
+                        ):
+                            # NOTE: Since this error can only be raised on the
+                            # ranks that have padding, this can manifest as a NCCL
+                            # watchdog timeout, as the other ranks will not error.
+                            raise AssertionError(
+                                "When a parameter is unevenly sharded by FSDP "
+                                f"(orig size={self._orig_size}, FSDP world size={self.mesh_info.mesh.size()}), "
+                                "fsdp_pre_all_gather must return all-gather inputs with the padded sharded size "
+                                f"{self.padded_sharded_param_size} but got {[t.size() for t in all_gather_inputs]}"
+                            )
+                    self._extensions_data.all_gather_input_sizes = [
+                        t.size() for t in all_gather_inputs
+                    ]
+                    return [t.view(-1) for t in all_gather_inputs]
+                sharded_param_data = self._sharded_param_data
+                if self.offload_to_cpu:
+                    sharded_param_data = sharded_param_data.to(
+                        self.device, non_blocking=True
+                    )
+                return [_to_dtype_if_needed(sharded_param_data, self.param_dtype)]
+            elif self.sharded_state == ShardedState.SHARDED_POST_FORWARD:
+                if not compiled_autograd_enabled() and hasattr(
+                    self._sharded_local_tensor, "fsdp_pre_all_gather"
+                ):
+                    raise NotImplementedError
+                all_gather_input = _to_dtype_if_needed(
+                    cast(torch.Tensor, self._sharded_post_forward_param_data),
+                    self.param_dtype,
+                )
+                return [all_gather_input]
+            return [torch.empty(0)]  # mypy
+
+        @property
+        def shard_mesh_from_root(self):
+            mesh = self.mesh_info.mesh
+
+            if mesh.ndim == 1:
+                return mesh
+            else:
+                assert mesh.mesh_dim_names is not None
+                shard_dim_name = mesh.mesh_dim_names[-1]
+
+                root_mesh = _mesh_resources.get_root_mesh(mesh)
+                return root_mesh[shard_dim_name]
+
+        FSDPParam.all_gather_inputs = all_gather_inputs
+        FSDPParam.shard_mesh_from_root = shard_mesh_from_root
+
     def convert(self, model: nn.Module):
         if not self.enabled:
             return
@@ -63,6 +183,8 @@ class BitNetConverter(ModelConverter):
         Note that today, only dynamic tensor scaling (the default) is supported.
         This will mutate the model inplace.
         """
+        self._patch_pytorch()
+
         from torchao import quantize_
         from torchao.prototype.quantized_training import bitnet_training
 

7b0050feb816c19049ea70a48786144982c754b0
rakkit <rakkit@users.noreply.github.com>
2025-04-16 15:45:45 +0200

    Add distributed Scion

--- torchtitan/optimizers/__init__.py
+++ torchtitan/optimizers/__init__.py
@@ -4,4 +4,5 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
+from .distributed_scion import DistributedScion  # noqa: F401
 from .scion import Scion  # noqa: F401
